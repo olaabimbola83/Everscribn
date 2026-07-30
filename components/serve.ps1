cc7b64fb1424374e78c98093453c71b363e48673
Vic-thore <olaabimbola83@gmail.com>
2026-07-30 09:16:41 +0100

    auto: update site content

--- components/serve.ps1
+++ components/serve.ps1
@@ -12,6 +12,7 @@ $mime = @{
     ".html" = "text/html"; ".htm" = "text/html"; ".css" = "text/css"
     ".js" = "application/javascript"; ".json" = "application/json"
     ".png" = "image/png"; ".jpg" = "image/jpeg"; ".jpeg" = "image/jpeg"
+    ".webp" = "image/webp"
     ".svg" = "image/svg+xml"; ".ico" = "image/x-icon"; ".xml" = "application/xml"
 }
 
